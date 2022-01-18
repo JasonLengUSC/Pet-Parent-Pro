93b5012f820e457256efe67a2c72d4a42ca3fe58
yww0225 <yw0225.career@gmail.com>
2022-01-18 14:18:42 -0800

    Reusable NavButton and style created.

---
+++ src/components/UI/NavButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./NavButton.module.css";
+
+const NavButton = (props) => {
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default NavButton;
