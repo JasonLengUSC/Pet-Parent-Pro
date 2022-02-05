a9fc76381ebc66c271ed1216d973b8b2d1911f2f
yww0225 <yw0225.career@gmail.com>
2022-02-04 18:39:28 -0800

    PPP-16:feature Dog form styling implemented

--- src/forms/DogFormStyles.js
+++ src/forms/DogFormStyles.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 import { Button } from "antd";
 
+const UploadButton = styled(Button)`
+  margin-left: 1em;
+`;
 const ButtonStyled = styled(Button)`
   margin-right: 8px;
   margin-bottom: 2em;
 `;
 
-export { ButtonStyled };
+export { UploadButton, ButtonStyled };
